e9ce102b8c2ca4b77c9dafa3a268f0b89d68ecb1
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-07-29 21:42:49 +0200

    powiadomienia

--- CzyDobre/Controllers/HomeController.cs
+++ CzyDobre/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CzyDobre.Extensions;
 
 namespace CzyDobre.Controllers
 {
@@ -11,7 +12,8 @@ namespace CzyDobre.Controllers
     {
         [AllowAnonymous]
         public ActionResult Index()
-        {
+        { 
+            this.AddNotification("Strona jest w budowie, jeżeli masz pomysł na nową funkcję i chcesz się tym z nami podzielić napisz do nas na kontakt@czydobre.pl ", NotificationType.WARNING);
             return View();
         }
 
