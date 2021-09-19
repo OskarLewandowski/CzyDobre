dbdc5c91aa36a9b6499191a2061eef4f43359fa4
Adrian Jabłecki <adrjab1@st.amu.edu.pl>
2021-09-19 14:02:34 +0200

    Zmiany

--- CzyDobre/Controllers/HomeController.cs
+++ CzyDobre/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using CzyDobre.Extensions;
+using CzyDobre.Models;
+
+
+
+
 namespace CzyDobre.Controllers
 {
 
     public class HomeController : Controller
     {
+       
         //CzyDobre.pl/
+
         [AllowAnonymous]
         public ActionResult Index()
         {
@@ -26,11 +36,58 @@ namespace CzyDobre.Controllers
 
         //CzyDobre.pl/kontakt
         [Route("kontakt")]
+
         [AllowAnonymous]
+        [HttpGet]
         public ActionResult Contact()
         {
             return View();
         }
+        [Route("kontakt")]
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
+                                                         //from contact us page 
+                    msz.To.Add("kontakt@czydobre.pl");//Where mail will be sent 
+                    msz.Subject = vm.Subject;
+                    msz.Body = vm.Message;
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.webio.pl";
+
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("kontakt@czydobre.pl", "Esl-123");
+
+                    
+                    smtp.Send(msz);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
+        }
+
+
+        // public ActionResult Contact()
+        // {
+        //     return View();
+        // }
 
         //CzyDobre.pl/opinie
         [Route("opinie")]
--- CzyDobre/Models/ContactModel.cs
+++ CzyDobre/Models/ContactModel.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 
 namespace CzyDobre.Models
 {
-    public class ContactModel
+    public class ContactViewModel
     {
-        [Required, Display(Name = "Twój Nick:")]
-        public string SenderName { get; set; }
-        [Required, Display(Name = "Twój Email:"), EmailAddress]
-        public string SenderEmail { get; set; }
-        [Required, Display(Name = "Treść wiadomości:")]
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
         public string Message { get; set; }
     }
 }
