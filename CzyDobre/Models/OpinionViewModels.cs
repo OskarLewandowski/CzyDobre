40c8b67021f233ac53b7e7094e4285771bd7f644
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-10-19 12:45:08 +0200

    uzupełnienie podstrony Opinion opiniami z bazy

---
+++ CzyDobre/Models/OpinionViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CzyDobre.Models
+{
+    public class OpinionViewModels
+    {
+        public string ProductName { get; set; }
+        public string RateService { get; set; }
+        public string RateTaste { get; set; }
+        public string RateComposition { get; set; }
+        public string RateIngredients { get; set; }
+        public string RateAdcompliance { get; set; }
+    }
+}
