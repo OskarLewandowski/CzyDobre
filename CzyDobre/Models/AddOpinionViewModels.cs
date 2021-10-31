a487772b1b089fdc55c2905dbcf76d56cc86f91d
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-10-31 23:20:17 +0100

    wysyłanie zdjec do cloudinary w dodaj opnie

---
+++ CzyDobre/Models/AddOpinionViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CzyDobre.Models
+{
+    public class AddOpinionViewModels
+    {
+        public List<HttpPostedFileBase> Photo { get; set; }
+    }
+}
