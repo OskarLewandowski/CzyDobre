911cbaf747c80cd8d1c9b7720d545e0f27ec29f8
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-11-22 13:34:51 +0100

    zmiany i ulepszenia

--- CzyDobre/Models/ContactUsViewModels.cs
+++ CzyDobre/Models/ContactUsViewModels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -9,27 +10,64 @@ namespace CzyDobre.Models
 {
     public class ContactUsViewModels
     {
-        [Required]
-        [StringLength(32, MinimumLength = 3)]
+        [Required(ErrorMessage = "Pole Nazwa jest wymagane")]
+        [StringLength(32, MinimumLength = 3, ErrorMessage = "Maksymalna dozwolona ilość znaków 32")]
         [Display(Name = "Nazwa")]
         public string Name { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [StringLength(32)]
+        [Required (ErrorMessage = "Pole Email jest wymagane")]
+        [EmailAddress (ErrorMessage = "Wymagany jest poprawny adres Email")]
+        [StringLength(64)]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(32)]
+        [Required (ErrorMessage = "Pole Temat jest wymagane")]
+        [StringLength(32 ,ErrorMessage = "Maksymalna dozwolona ilość znaków to 32")]
         [Display(Name = "Temat")]
         public string Subject { get; set; }
 
-        [Required]
-        [StringLength(255)]
+        [Required (ErrorMessage = "Pole Treść wiadomości jest wymagane")]
+        [StringLength(255 , ErrorMessage ="Maksymalna dozwolona ilość znaków to 255")]
         [Display(Name = "Treść wiadomości")]
         public string Message { get; set; }
 
+        [AttachmentCheck]
         public List<HttpPostedFileBase> Attachment { get; set; }
+    }
+
+    public class AttachmentCheck : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            bool OK = false;
+            int allSize = 0;
+            var file = (ContactUsViewModels)validationContext.ObjectInstance;
+
+            foreach (HttpPostedFileBase item in file.Attachment)
+            {
+                if (item != null && item.ContentLength > 0)
+                {
+                    var byteCount = item.ContentLength;
+
+                    allSize = allSize + byteCount;
+
+                    if (allSize < 5242880)
+                    {
+                        OK = true;
+                    }
+                    else
+                    {
+                        OK = false;
+                        return new ValidationResult("Pliki ważą za dużo! Maksymalna wartość załącznika wynosi 5MB");
+                    }
+                }
+            }
+
+            if (OK == true)
+            {
+                return ValidationResult.Success;
+            }
+            return ValidationResult.Success;
+        }
     }
 }
