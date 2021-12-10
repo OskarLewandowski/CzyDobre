2dfb10d694210a3e053df99258df4bb96788887f
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-12-10 17:27:26 +0100

    wstępny panel zarzadzania produktem

---
+++ CzyDobre/Controllers/ProduktController.cs
@@ -0,0 +1,22 @@
+﻿using CzyDobre.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CzyDobre.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProduktController : Controller
+    {
+        DBEntities db = new DBEntities();
+
+        // GET: Produkt
+        public ActionResult ProduktList()
+        {
+            var produktList = db.AspNetProducts.ToList();
+            return View(produktList);
+        }
+    }
+}
