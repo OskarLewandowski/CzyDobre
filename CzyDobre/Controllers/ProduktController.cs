6ab855f9420818a53f792199821a912e34a57478
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-12-10 21:19:52 +0100

    usuwanie z cluda

--- CzyDobre/Controllers/ProduktController.cs
+++ CzyDobre/Controllers/ProduktController.cs
@@ -1,7 +1,10 @@
-﻿using CzyDobre.Extensions;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using CzyDobre.Extensions;
 using CzyDobre.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -49,18 +52,43 @@ namespace CzyDobre.Controllers
         [Route("usuwanie-produktu")]
         [Route("Produkt/Delete")]
         [Authorize(Roles = "Admin")]
-        public ActionResult Delete(int idProdukt, string nazwaProduktu)
+        public ActionResult Delete(int idProdukt, string nazwaProduktu, string imageUrl)
         {
             try
             {
+                Account account = new Account(
+                ConfigurationManager.AppSettings["CloudinaryName"].ToString(),
+                ConfigurationManager.AppSettings["CloudinaryApiKey"].ToString(),
+                ConfigurationManager.AppSettings["CloudinaryApiSecret"].ToString());
+                Cloudinary cloudinary = new Cloudinary(account);
+
                 var idProduktu = db.AspNetProducts.Where(m => m.Id_Product == idProdukt).FirstOrDefault();
                 var idZdjecia = db.AspNetImages.Where(m => m.Id_Product == idProdukt).FirstOrDefault();
                 if (idProduktu != null && idZdjecia != null)
                 {
                     db.AspNetProducts.Remove(idProduktu);
                     db.AspNetImages.Remove(idZdjecia);
+
                     db.SaveChanges();
-                    this.AddNotification("Produkt \"" + nazwaProduktu + "\" został pomyślnie usunięty!", NotificationType.SUCCESS);
+                    //usuwan rozszezenia .jpg 4 i .jpng 5
+                    var ID4 = imageUrl.Remove(imageUrl.Length - 4);
+                    var ID5 = imageUrl.Remove(imageUrl.Length - 5);
+                    var nameId4 = "CzyDobre-images/" + ID4;
+                    var nameId5 = "CzyDobre-images/" + ID5;
+
+                    var deletionParams4 = new DeletionParams(ID4)
+                    {
+                        PublicId = nameId4
+                    };
+                    var deletionResult = cloudinary.Destroy(deletionParams4);
+
+                    var deletionParams5 = new DeletionParams(ID5)
+                    {
+                        PublicId = nameId5
+                    };
+                    var deletionResult4 = cloudinary.Destroy(deletionParams5);
+
+                    this.AddNotification("Produkt \"" + nazwaProduktu + "\" został pomyślnie usunięty! " + ID4 + ID5, NotificationType.SUCCESS);
                 }
                 else
                 {
