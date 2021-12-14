9c871c87baafdd93a0f263ce90fe3e0fcf19bf7c
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-12-14 19:26:45 +0100

    naprawa porawa zmian

--- CzyDobre/Controllers/OpiniaController.cs
+++ CzyDobre/Controllers/OpiniaController.cs
@@ -166,57 +166,52 @@ namespace CzyDobre.Controllers
                 ConfigurationManager.AppSettings["CloudinaryApiSecret"].ToString());
                 Cloudinary cloudinary = new Cloudinary(account);
 
-                var OpiniaID = db.AspNetRatings.Where(m => m.Id_Rating == idRating).FirstOrDefault();
-                var listaZdjecID = db.AspNetRatingPictures.Where(u => u.Id_Rating == idRating).Select(u => u.Id_Picture).ToList();
-                var listaZdjecUrl = db.AspNetRatingPictures.Where(u => u.Id_Rating == idRating).Select(u => u.Url).ToList();
-                var idZdjecie = db.AspNetRatingPictures.Where(m => m.Id_Picture == idProduct).FirstOrDefault();
-
-                // var idOpini = db.AspNetRatings.Where(m => m.Id_Product == idProduct).Select(m => m.Id_Rating).FirstOrDefault();
-                // var idZdjecie = db.AspNetRatingPictures.Where(m => m.Id_Rating == idOpini).ToList();
-
-
-                if (OpiniaID != null)
+                var Opinia = db.AspNetRatings.Where(m => m.Id_Rating == idRating).ToList();
+          
+                foreach (AspNetRating r in Opinia)
                 {
-                    if (listaZdjecID != null)
+                    lista.AddRange(db.AspNetRatingPictures.Where(m => m.Id_Rating == r.Id_Rating).ToList());
+                }
+
+                if (Opinia != null)
+                {
+                    if (lista != null)
                     {
-                        for (int i = 0; i < listaZdjecID.Count; i++)
-                        {
-                            idZdjecie = db.AspNetRatingPictures.Where(m => m.Id_Picture == listaZdjecID[i]).FirstOrDefault();
-                            if (idZdjecie != null)
-                            {
-                                db.AspNetRatingPictures.Remove(idZdjecie);
-                            }
-                        }
+                        db.AspNetRatingPictures.RemoveRange(lista);
                     }
 
-                    if (OpiniaID != null)
+                    if (Opinia != null)
                     {
-                        db.AspNetRatings.Remove(OpiniaID);
+                        db.AspNetRatings.RemoveRange(Opinia);
                     }
 
                     db.SaveChanges();
-                    //this.AddNotification(" "+Produkt.ToString()+"  "+ZdjecieProduktu.ToString() + " "+ Opinia.ToString() + " "+idZdjecie.ToString() + " "+idOpini.ToString(), NotificationType.INFO);
 
-                    //if (idZdjecie != null)
-                    //{
-                    //    //usuwan rozszezenia .jpg 4 i .jpng 5
-                    //    var ID4 = imageUrl.Remove(imageUrl.Length - 4);
-                    //    var ID5 = imageUrl.Remove(imageUrl.Length - 5);
-                    //    var nameId4 = "CzyDobre-images/" + ID4;
-                    //    var nameId5 = "CzyDobre-images/" + ID5;
+                    var listaZdjecUrl = db.AspNetRatingPictures.Where(u => u.Id_Rating == idRating).Select(u => u.Url).ToList();
 
-                    //    var deletionParams4 = new DeletionParams(ID4)
-                    //    {
-                    //        PublicId = nameId4
-                    //    };
-                    //    var deletionResult = cloudinary.Destroy(deletionParams4);
+                    if (listaZdjecUrl != null)
+                    {
+                        for (int i = 0; i < listaZdjecUrl.Count; i++)
+                        {
+                            //usuwan rozszezenia .jpg 4 i .jpng 5
+                            var ID4 = listaZdjecUrl[i].Remove(listaZdjecUrl[i].Length - 4);
+                            var ID5 = listaZdjecUrl[i].Remove(listaZdjecUrl[i].Length - 5);
+                            var nameId4 = "CzyDobre-images/" + ID4;
+                            var nameId5 = "CzyDobre-images/" + ID5;
 
-                    //    var deletionParams5 = new DeletionParams(ID5)
-                    //    {
-                    //        PublicId = nameId5
-                    //    };
-                    //    var deletionResult4 = cloudinary.Destroy(deletionParams5);
-                    //}
+                            var deletionParams4 = new DeletionParams(ID4)
+                            {
+                                PublicId = nameId4
+                            };
+                            var deletionResult = cloudinary.Destroy(deletionParams4);
+
+                            var deletionParams5 = new DeletionParams(ID5)
+                            {
+                                PublicId = nameId5
+                            };
+                            var deletionResult4 = cloudinary.Destroy(deletionParams5);
+                        }
+                    }
 
                     this.AddNotification("Opinia \"" + nazwaProduktu + "\" została pomyślnie usunięta! ", NotificationType.SUCCESS);
                 }
