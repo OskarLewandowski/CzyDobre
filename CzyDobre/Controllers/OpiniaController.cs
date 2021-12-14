5da8b23d2a9b4601027dca2407413ae38a09d981
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-12-14 14:53:38 +0100

    dodanie strony zarzadzania opiniami

---
+++ CzyDobre/Controllers/OpiniaController.cs
@@ -0,0 +1,24 @@
+﻿using CzyDobre.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CzyDobre.Controllers
+{
+    [Authorize(Roles = "Admin, Moderator")]
+    public class OpiniaController : Controller
+    {
+        DBEntities db = new DBEntities();
+
+        [Route("lista-opinii")]
+        [Route("Opinia/OpiniaList")]
+        [Authorize(Roles = "Admin, Moderator")]
+        public ActionResult OpiniaList()
+        {
+            var produktList = db.AspNetRatings.ToList();
+            return View(produktList);
+        }
+    }
+}
