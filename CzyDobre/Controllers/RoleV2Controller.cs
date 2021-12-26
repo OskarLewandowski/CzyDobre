09de78f2ec927673b0a58780bc49b296d510cf59
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-12-26 16:44:34 +0100

    rola 2.0 alpha

---
+++ CzyDobre/Controllers/RoleV2Controller.cs
@@ -0,0 +1,24 @@
+﻿using CzyDobre.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CzyDobre.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleV2Controller : Controller
+    {
+        DBEntities db = new DBEntities();
+
+        [Route("lista-roli")]
+        [Route("RoleV2/RoleList")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult RoleList()
+        {
+            var userList = db.AspNetUsers.ToList();
+            return View(userList);
+        }
+    }
+}
