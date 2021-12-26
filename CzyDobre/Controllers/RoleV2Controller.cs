0c63d3241c137128f4a108572195cc013fae5697
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2021-12-26 17:22:52 +0100

    dodawanie nowej roli

--- CzyDobre/Controllers/RoleV2Controller.cs
+++ CzyDobre/Controllers/RoleV2Controller.cs
@@ -1,4 +1,6 @@
-﻿using CzyDobre.Models;
+﻿using CzyDobre.Extensions;
+using CzyDobre.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,13 @@ namespace CzyDobre.Controllers
     {
         DBEntities db = new DBEntities();
 
+        ApplicationDbContext _context;
+
+        public RoleV2Controller()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [Route("lista-roli")]
         [Route("RoleV2/RoleList")]
         [Authorize(Roles = "Admin")]
@@ -20,5 +29,56 @@ namespace CzyDobre.Controllers
             var userList = db.AspNetUsers.ToList();
             return View(userList);
         }
+
+        [Route("tworzenie-nowej-roli")]
+        [Route("RoleV2/Create")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [Route("tworzenie-nowej-roli")]
+        [Route("RoleV2/Create")]
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            var nameRole = Role.Name;
+            bool uniq = false;
+            var wynik = db.AspNetRoles.Where(u => u.Name == nameRole).Select(u => u.Name).FirstOrDefault();
+
+            if (wynik == "" || wynik == null)
+            {
+                uniq = true;
+            }
+            else
+            {
+                uniq = false;
+                this.AddNotification("Taka nazwa roli już istnieje!", NotificationType.ERROR);
+                return RedirectToAction("Create", "RoleV2");
+            }
+
+            if (nameRole != null && uniq == true)
+            {
+                _context.Roles.Add(Role);
+                _context.SaveChanges();
+                this.AddNotification("Rola \"" + nameRole + "\" dodana!", NotificationType.SUCCESS);
+                return RedirectToAction("Create", "RoleV2");
+            }
+            else if(nameRole == null)
+            {
+                this.AddNotification("Nazwa roli jest wymagana, pole nie może być puste!", NotificationType.ERROR);
+                return RedirectToAction("Create", "RoleV2");
+            }
+            else
+            {
+                this.AddNotification("Ups! Coś poszło nie tak", NotificationType.ERROR);
+                return RedirectToAction("Create", "RoleV2");
+            }
+        }
+
+
     }
 }
