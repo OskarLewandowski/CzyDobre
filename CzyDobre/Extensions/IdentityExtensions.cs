079afcf254c5771f7c0c11231ff50377c9b526e3
Marek Rymarkiewicz <m.rymarkiewicz@op.pl>
2021-12-29 22:31:35 +0100

    Przywrócenie oryginalnego wyglądy wyszukiwarki. Drobne przemiany stylistyczne w Navbar. Ustawienie domyślnego avatara.

--- CzyDobre/Extensions/IdentityExtensions.cs
+++ CzyDobre/Extensions/IdentityExtensions.cs
@@ -20,7 +20,7 @@ namespace CzyDobre.IdentityExtensions
             }
             else
             {
-                return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAgZJREFUeF7tmEuLwkAQhFtBDYKIkXjwgXjx4v//F15z8SKBJIhv0YPgYZeeZZZJdlYnExVJam6SSXq6Ul0fpjKfz7+oxKsCAeAAjAAyoMQZSAhBUAAUAAVAAVCgxAoAg8AgMAgMAoMlhgD+DAGDwCAwCAwCg8CghQKz2YwcxxF3ns9nWiwWiad0u10ajUZ0OBwoCAKLCj+3vLqOFQan06lofrlcUr1ep+FwSJvNhuI4/m1UHny73VoL8I46mQVotVo0Ho9pv98nGlZfMV9vt9tUrVa1DmBxbrebcA0/bzKZ0Ol0Sgj1jDomtsssAFt7MBiIBprN5p8R4OvsiOPxSJ1ORyuA2lytVqNGo6Edobx1XiYAzzYL4Ps+9ft96vV6tF6vhSPYtrx2u93dDOD7PM8Te8MwFPvVJTMkb51HIlg5QJ15aeHr9UqXy4Vc1xVW5mx4FILqKKQPKp0ksyVPnXsiZBYgPbPqwbgQ/04vXRByTsi9ujx5Vp2nO4AfyDbn2eUR4Eb4ba1Wq0Qo3sOgzJEoisT8S9cwTtWVt86j5vl6ZgfIh6p81r3h/wTQpb7aaPrQtnVMms8lgGmBT99n7YBPb8z0fBAAH0TwQQQfRPBBxDQxi7gPFAAFQAFQABQoYrqb9gQKgAKgACgACpgmZhH3gQKgACgACoACRUx3055AAVCg5BT4BmBdKR+GxUqvAAAAAElFTkSuQmCC";
+                return "https://res.cloudinary.com/czydobre-pl/image/upload/v1640786753/CzyDobre-www/awatar-domyslny-ramka_u9xv4t.png";
             }
         }
     }
